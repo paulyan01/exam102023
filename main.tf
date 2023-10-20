b074d1015a55740bac78b5f7ec05a5a841ac205f
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-20 19:25:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ locals{
   clusters_name= ["MontrealCollegeCluster1","MontrealCollegeCluster2","MontrealCollegeCluster3","MontrealCollegeCluster4"]
 
 } 
-resource "azurerm_resource_group" "examoct2023" {
-  name     = "MCITEXAM"
-  location = "Central Canada"
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "MCIT_resource_group"
+  location = "Canada Central"
 }
 
 resource "azurerm_kubernetes_cluster" "k8scluster" {
   for_each            ={for cluster in local.clusters_name:cluster=>cluster}
   name                = "MontrealCollege"
-  location            = azurerm_resource_group.examoct2023.location
-  resource_group_name = azurerm_resource_group.examoct2023.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
